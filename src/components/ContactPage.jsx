bfc32f4c822f96672b768535abecab08b9d0992a
kunal <chaudharykunal964@gmail.com>
2025-06-20 15:27:44 +0530

    Add _redirects file to fix Netlify 404 routing

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import ContactForm from './ContactForm';
 
-
 const ContactPage = () => {
   return (
-    <div className="pt-16 min-h-screen bg-white dark:bg-zinc-900 flex flex-col md:flex-row items-center justify-center p-6 gap-10">
-      
-      {/* Left Side - Contact Form */}
-      <div className="w-full md:w-1/2">
-        <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-6">Contact Us</h1>
-        <p className="text-gray-600 dark:text-gray-300 mb-4">
-          We'd love to hear from you! Fill out the form and our team will get in touch shortly.
-        </p>
-        <ContactForm onClose={() => {}} /> {/* `onClose` won't be used here */}
-      </div>
+    <div className="pt-16 min-h-screen overflow-y-auto bg-white dark:bg-zinc-900 px-6">
+      <div className="max-w-screen-xl mx-auto flex flex-col md:flex-row items-center justify-center gap-10">
+        
+        {/* Left Side - Contact Form */}
+        <div className="w-full md:w-1/2">
+          <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-6">Contact Us</h1>
+          <p className="text-gray-600 dark:text-gray-300 mb-4">
+            We'd love to hear from you! Fill out the form and our team will get in touch shortly.
+          </p>
+          <ContactForm onClose={() => {}} />
+        </div>
 
-      {/* Right Side - Overlayed Images */}
-      <div className="w-full md:w-1/2 relative h-[500px]">
-        <img
-          src="/images/Office_1.jpg"
-          alt="Virtual Office"
-          className="rounded-lg shadow-lg w-full h-full object-cover"
-        />
-
-        {/* Add more overlays as needed */}
+        {/* Right Side - Image */}
+        <div className="w-full md:w-1/2 relative h-[500px]">
+          <img
+            src="/images/Office_1.jpg"
+            alt="Virtual Office"
+            className="rounded-lg shadow-lg w-full h-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
